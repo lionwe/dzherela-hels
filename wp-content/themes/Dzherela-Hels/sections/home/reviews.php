e5fd79ddf691d763b28299b7c7b9d4921dbdb9cd
Lionwe <lionwe433@gmail.com>
2026-01-28 11:58:53 +0200

    feat: add new section reviews

---
+++ wp-content/themes/Dzherela-Hels/sections/home/reviews.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Reviews Section
+ * 
+ * @package Dzherela-Hels
+ */
+?>
+
+<section class="reviews">
+    <div class="container">
+        <div class="reviews__header">
+            <h2 class="reviews__title">Відгуки</h2>
+            <span class="reviews__underline"></span>
+        </div>
+
+        <div class="reviews__content">
+            <?php echo do_shortcode('[trustindex no-registration=google]'); ?>
+        </div>
+    </div>
+</section>
